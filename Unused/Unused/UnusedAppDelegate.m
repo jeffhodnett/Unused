0e3f420a6c35d4774852fa0f8c25befbf27ee21f
Andre Cytryn <kinhow.cyt@gmail.com>
2013-12-04 18:52:08 -0200

    Added search on Storyboards; Changed .gitignore; Fixes #8

--- Unused/Unused/UnusedAppDelegate.m
+++ Unused/Unused/UnusedAppDelegate.m
@@ -18,6 +18,7 @@
 @synthesize window=_window;
 @synthesize mCheckbox=_mCheckbox;
 @synthesize xibCheckbox=_xibCheckbox;
+@synthesize sbCheckbox=_sbCheckbox;
 @synthesize cppCheckbox=_cppCheckbox;
 @synthesize headerCheckbox=_headerCheckbox;
 @synthesize mmCheckbox=_mmCheckbox;
@@ -39,19 +40,19 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 {
     // Setup the results array
     _results = [[NSMutableArray alloc] init];
-
+    
     // Setup the retina images array
     _retinaImagePaths = [[NSMutableArray alloc] init];
-
+    
     // Setup the queue
     _queue = [[NSOperationQueue alloc] init];
-
+    
     // Setup double click
     [_resultsTableView setDoubleAction:@selector(tableViewDoubleClicked)];
-
+    
     // Setup labels
     [_statusLabel setTextColor:[NSColor lightGrayColor]];
-
+    
     // Setup search button
     [_searchButton setBezelStyle:NSRoundedBezelStyle];
     [_searchButton setKeyEquivalent:@"\r"];
@@ -67,7 +68,7 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
     [_results release];
     [_retinaImagePaths release];
     [_queue release];
-
+    
     [super dealloc];
 }
 
@@ -76,15 +77,15 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 {
     // Show an open panel
     NSOpenPanel *openPanel = [NSOpenPanel openPanel];
-
+    
     [openPanel setCanChooseDirectories:YES];
     [openPanel setCanChooseFiles:NO];
-
+    
     NSInteger option = [openPanel runModal];
     if (option == NSOKButton) {
         // Store the path
         self.searchDirectoryPath = [[openPanel directoryURL] path];
-
+        
         // Update the path text field
         [self.pathTextField setStringValue:self.searchDirectoryPath];
     }
@@ -95,26 +96,29 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
     NSSavePanel *save = [NSSavePanel savePanel];
     [save setAllowedFileTypes:[NSArray arrayWithObject:@"txt"]];
     NSInteger result = [save runModal];
-
+    
     if (result == NSOKButton) {
         NSString *selectedFile = [[save URL] path];
-
+        
         NSMutableString *outputResults = [[NSMutableString alloc] init];
         [outputResults appendFormat:NSLocalizedString(@"ExportSummaryTitle", @""), self.searchDirectoryPath];
-
+        
         for (NSString *path in _results) {
             [outputResults appendFormat:@"%@\n",path];
         }
-
+        
         // Output
         [outputResults writeToFile:selectedFile atomically:YES encoding:NSUTF8StringEncoding error:nil];
-
+        
         [outputResults release];
     }
 }
 
 -(IBAction)startSearch:(id)sender
 {
+    // Update the path text field
+    [self.pathTextField setStringValue:self.searchDirectoryPath];
+    
     // Check for a path
     if(!self.searchDirectoryPath) {
         // Show an alert
@@ -122,34 +126,34 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
         [alert setMessageText:NSLocalizedString(@"ProjectPathErrorTitle", @"")];
         [alert setInformativeText:NSLocalizedString(@"PleaseSelectValidPathErrorMessage", @"")];
         [alert runModal];
-
+        
         return;
     }
-
+    
     // Check the path
     if(![[NSFileManager defaultManager] fileExistsAtPath:self.searchDirectoryPath]) {
         NSAlert *alert = [[[NSAlert alloc] init] autorelease];
         [alert setMessageText:NSLocalizedString(@"ProjectPathErrorTitle", @"")];
         [alert setInformativeText:NSLocalizedString(@"InvalidFolderPathErrorMessage", @"")];
         [alert runModal];
-
+        
         return;
     }
-
+    
     // Change the button text
     [_searchButton setEnabled:NO];
     [_searchButton setKeyEquivalent:@""];
-
+    
     // Reset
     [_results removeAllObjects];
     [_retinaImagePaths removeAllObjects];
     [_resultsTableView reloadData];
-
+    
     // Start the search
     NSInvocationOperation *op = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(runImageSearch) object:nil];
     [_queue addOperation:op];
     [op release];
-
+    
     isSearching = YES;
 }
 
@@ -157,17 +161,17 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 {
     // Start the ui
     [self setUIEnabled:NO];
-
+    
     // Find all the .png files in the folder
     [_pngFiles release];
     _pngFiles = [[self pngFilesAtDirectory:_searchDirectoryPath] retain];
-
+    
     NSArray *pngFiles = _pngFiles;
-
+    
     if (SHOULD_FILTER_ENUM_VARIANTS)
     {
         NSMutableArray *mutablePngFiles = [NSMutableArray arrayWithArray:pngFiles];
-
+        
         // Trying to filter image names like: "Section_0.png", "Section_1.png", etc (these names can possibly be created by [NSString stringWithFormat:@"Section_%d", (int)] constructions) to just "Section_" item
         for (NSInteger index = 0, count = [mutablePngFiles count]; index < count; index++)
         {
@@ -177,15 +181,15 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
             if (newImageName != nil)
                 [mutablePngFiles replaceObjectAtIndex:index withObject:newImageName];
         }
-
+        
         // Remove duplicates and update pngFiles array
         pngFiles = [[NSSet setWithArray:mutablePngFiles] allObjects];
     }
-
+    
     // Setup all the @2x image firstly
     for (NSString *pngPath in _pngFiles) {
         NSString *imageName = [pngPath lastPathComponent];
-
+        
         // Does the image have a @2x
         NSRange retinaRange = [imageName rangeOfString:@"@2x"];
         if(retinaRange.location != NSNotFound) {
@@ -193,15 +197,15 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
             [_retinaImagePaths addObject:pngPath];
         }
     }
-
+    
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_group_t group = dispatch_group_create();
-        
+    
     // Now loop and check
     [pngFiles enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         dispatch_group_async(group, queue, ^{
             NSString *pngPath = (NSString *)obj;
-             
+            
             // Check that the png path is not empty
             if(![pngPath isEqualToString:@""]) {
                 // Grab the file name
@@ -209,20 +213,21 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
                 
                 // Check that it's not a @2x or reserved image name
                 if([self isValidImageAtPath:pngPath]) {
-                                        
+                    
                     // Settings items
                     NSArray *settingsItems = [NSArray arrayWithObjects:
                                               [NSDictionary dictionaryWithObjectsAndKeys:_mCheckbox, kSettingControlKey, @"m", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_xibCheckbox, kSettingControlKey, @"xib", kSettingExtensionKey, nil],
+                                              [NSDictionary dictionaryWithObjectsAndKeys:_sbCheckbox, kSettingControlKey, @"storyboard", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_cppCheckbox, kSettingControlKey, @"cpp", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_mmCheckbox, kSettingControlKey, @"mm", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_htmlCheckbox, kSettingControlKey, @"html", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_cssCheckbox, kSettingControlKey, @"css", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_plistCheckbox, kSettingControlKey, @"plist", kSettingExtensionKey, nil],
                                               [NSDictionary dictionaryWithObjectsAndKeys:_headerCheckbox,
-                                                  kSettingControlKey, @"h", kSettingExtensionKey, nil],
-
-                                       nil];
+                                               kSettingControlKey, @"h", kSettingExtensionKey, nil],
+                                              
+                                              nil];
                     BOOL isSearchCancelled = NO;
                     for (NSDictionary *settingDic in settingsItems) {
                         // Get the items
@@ -235,41 +240,41 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
                             isSearchCancelled = YES;
                         }
                     }
-                                        
+                    
                     // Is it not found
                     // Update results
                     if (!isSearchCancelled)
                         dispatch_async(dispatch_get_main_queue(), ^{
                             [self addNewResult:pngPath];
                         });
-                 }
-             }
-         });
-     }];
+                }
+            }
+        });
+    }];
     
     dispatch_group_notify(group, queue, ^
-    {
-        dispatch_async(dispatch_get_main_queue(), ^
-        {
-            // Sorting results and refreshing table
-            [_results sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-            [_resultsTableView reloadData];
-            
-            // Calculate how much file size we saved and update the label
-            int fileSize = 0;
-            for (NSString *path in _results) {
-                fileSize += [[[NSFileManager defaultManager] attributesOfItemAtPath:path error:nil] fileSize];
-            }
-            
-            [_statusLabel setStringValue:[NSString stringWithFormat:NSLocalizedString(@"CompletedResultMessage", @""), (unsigned long)[_results count], [self stringFromFileSize:fileSize]]];
-            
-            // Enable the ui
-            [self setUIEnabled:YES];
-            
-            isSearching = NO;
-            [_fileData removeAllObjects];
-        });
-    });
+                          {
+                              dispatch_async(dispatch_get_main_queue(), ^
+                                             {
+                                                 // Sorting results and refreshing table
+                                                 [_results sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+                                                 [_resultsTableView reloadData];
+                                                 
+                                                 // Calculate how much file size we saved and update the label
+                                                 int fileSize = 0;
+                                                 for (NSString *path in _results) {
+                                                     fileSize += [[[NSFileManager defaultManager] attributesOfItemAtPath:path error:nil] fileSize];
+                                                 }
+                                                 
+                                                 [_statusLabel setStringValue:[NSString stringWithFormat:NSLocalizedString(@"CompletedResultMessage", @""), (unsigned long)[_results count], [self stringFromFileSize:fileSize]]];
+                                                 
+                                                 // Enable the ui
+                                                 [self setUIEnabled:YES];
+                                                 
+                                                 isSearching = NO;
+                                                 [_fileData removeAllObjects];
+                                             });
+                          });
 }
 
 -(void)setUIEnabled:(BOOL)state
@@ -290,6 +295,7 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
     [_processIndicator setHidden:state];
     [_mCheckbox setEnabled:state];
     [_xibCheckbox setEnabled:state];
+    [_sbCheckbox setEnabled:state];
     [_cppCheckbox setEnabled:state];
     [_mmCheckbox setEnabled:state];
     [_headerCheckbox setEnabled:state];
@@ -306,57 +312,57 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
     // Create a find task
     NSTask *task = [[[NSTask alloc] init] autorelease];
     [task setLaunchPath: @"/usr/bin/find"];
-
+    
     // Search for all png files
     NSArray *argvals = [NSArray arrayWithObjects:directoryPath,@"-name",@"*.png", nil];
     [task setArguments: argvals];
-
+    
     NSPipe *pipe = [NSPipe pipe];
     [task setStandardOutput: pipe];
-
+    
     NSFileHandle *file;
     file = [pipe fileHandleForReading];
-
+    
     [task launch];
-
+    
     // Read the response
     NSData *data;
     data = [file readDataToEndOfFile];
-
+    
     NSString *string;
     string = [[[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding] autorelease];
-
+    
     // See if we can create a lines array
     NSArray *lines = [string componentsSeparatedByString:@"\n"];
-
+    
     return lines;
 }
 
 -(BOOL)isValidImageAtPath:(NSString *)imagePath
 {
     NSString *imageName = [imagePath lastPathComponent];
-
+    
     // Does the image have a @2x
     NSRange retinaRange = [imageName rangeOfString:@"@2x"];
     if(retinaRange.location != NSNotFound) {
         return NO;
     }
-
+    
     // Is the name a part of 3rd party bundle
     if([imagePath rangeOfString:@".bundle"].length > 0) {
         return NO;
     }
-
+    
     // Is the name is Default
     if([imageName isEqualToString:@"Default.png"]) {
         return NO;
     }
-
+    
     // Is the name Icon
     if([imageName isEqualToString:@"Icon.png"] || [imageName isEqualToString:@"Icon@2x.png"] || [imageName isEqualToString:@"Icon-72.png"]) {
         return NO;
     }
-
+    
     // Is it a universal image
     if([imagePath rangeOfString:@"~ipad" options:NSCaseInsensitiveSearch].length > 0) {
         return NO;
@@ -367,7 +373,7 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 
 -(int)occurancesOfImageNamed:(NSString *)imageName atDirectory:(NSString *)directoryPath inFileExtensionType:(NSString *)extension
 {
-   // NSLog(@"%@", imageName);
+    // NSLog(@"%@", imageName);
     NSData *data;
 	[_fileDataLock lock];
 	data = [_fileData objectForKey:directoryPath];
@@ -400,10 +406,10 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 		[_fileData setObject:data forKey:key];
 	}
 	[_fileDataLock unlock];
-
+    
     NSString *string;
     string = [[[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding] autorelease];
-
+    
     // Calculate the count
     NSScanner *scanner = [NSScanner scannerWithString: string];
     NSCharacterSet *newline = [NSCharacterSet newlineCharacterSet];
@@ -411,7 +417,7 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
     while ([scanner scanUpToCharactersFromSet: newline  intoString: nil]) {
         count++;
     }
-
+    
     return count;
 }
 
@@ -419,32 +425,32 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 {
     if ([_pngFiles indexOfObject:pngPath] == NSNotFound)
         return;
-
+    
     // Add and reload
     [_results addObject:pngPath];
-
+    
     // Check for an @2x image too!
     for (NSString *retinaPath in _retinaImagePaths) {
-
+        
         // Compare the image name and the retina image name
         NSString *imageName = [pngPath lastPathComponent];
         imageName = [imageName stringByDeletingPathExtension];
         NSString *retinaImageName = [retinaPath lastPathComponent];
         retinaImageName = [retinaImageName stringByDeletingPathExtension];
         retinaImageName = [retinaImageName stringByReplacingOccurrencesOfString:@"@2x" withString:@""];
-
+        
         // Check
         if([imageName isEqualToString:retinaImageName]) {
             // Add it
             [_results addObject:retinaPath];
-
+            
             break;
         }
     }
-
+    
     // Reload
     [_resultsTableView reloadData];
-
+    
     // Scroll to the bottom
     NSInteger numberOfRows = [_resultsTableView numberOfRows];
     if (numberOfRows > 0)
@@ -474,7 +480,7 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
         NSString *imageName = [pngPath lastPathComponent];
         return imageName;
     }
-
+    
     return pngPath;
 }
 
@@ -497,9 +503,9 @@ NSString const *kSettingExtensionKey = @"kSettingExtensionKey";
 	if (floatSize<1023)
 		return([NSString stringWithFormat:@"%1.1f MB",floatSize]);
 	floatSize = floatSize / 1024;
-
+    
 	// Add as many as you like
-
+    
 	return([NSString stringWithFormat:@"%1.1f GB",floatSize]);
 }
 
